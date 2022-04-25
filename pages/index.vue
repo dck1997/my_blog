3f9eb4b2d1f4c6462f1b282d911555c6fbee23d5
Evan <1326587277@qq.com>
2022-04-25 16:39:48 +0800

    create

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <TopNav/>
+    <div class="content">
+      <card/>
+      <Article/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.content{
+  width: 80%;
+  height: 100%;
+  margin-left: 10%;
+  margin-top: 5%;
+  padding: 20px;
+  background: darkgrey;
+  position: absolute;
+  border-radius: 10px;
+}
+</style>
